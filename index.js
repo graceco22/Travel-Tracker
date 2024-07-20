6fc48c3a6e7d901ff64c14a1e84eeb8d0a614339
graceco22 <graceco921@gmail.com>
2024-07-20 16:57:40 -0700

    allow user to insert to pgAdmin countries

--- index.js
+++ index.js
@@ -28,9 +28,27 @@ app.get("/", async (req, res) => {
     result.rows.forEach((country) => {
       countries.push(country.country_code);
     });
-    console.log(result.rows);
     res.render("index.ejs", { countries: countries, total: countries.length});
-    db.end();
+});
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
